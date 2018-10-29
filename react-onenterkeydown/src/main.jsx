58e3f306a1cbd173d382cf30301af3663572910b
Julian Grinblat <julian@dotcore.co.il>
2018-10-29 16:42:37 +0900

    Move react-onenterkeydown to codesandbox example

--- react-onenterkeydown/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InputOnEnterDemo from './demo.jsx';
-
-ReactDOM.render(
-  <InputOnEnterDemo />,
-  document.getElementById('demo')
-);
