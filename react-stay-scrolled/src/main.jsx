f706059d12e7cf5521a6df0c51201765fb048525
Julian Grinblat <julian@dotcore.co.il>
2018-10-29 23:43:05 +0900

    Replace react-stay-scrolled demo with codesandbox

--- react-stay-scrolled/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StayScrolledDemo from './demo.jsx';
-
-ReactDOM.render(
-  <StayScrolledDemo />,
-  document.getElementById('demo')
-);
